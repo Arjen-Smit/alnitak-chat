e3a5b714186386d487a7bd4d8eb2cc95de23e316
Arjen Smit <arjen@alnitak.nl>
2015-04-05 23:27:18 +0200

    All in days work

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,77 @@
-/* Variables: */
-var     documentRoot = "public/";
-var 	sourceRoot = "src/";
-var     assetsDirectory = documentRoot + "assets/";
-var     productionDefualtEnviorment = true;
+/* Config: */
+var config = {
+	publicDir: "public/",
+	sourceDir: "src/",
+	bowerDir: "bower_components/",
+	assetsDir: "public/assets/",
+	productionEnvironment: true
+}
 
 /* Gulp requirements */
 var     gulp = require('gulp'),
         gutil = require('gulp-util'),
+        clean = require('gulp-clean'),
         uglify = require('gulp-uglify'),
         plumber = require('gulp-plumber'),
         concat = require('gulp-concat'),
         sourcemaps = require ('gulp-sourcemaps'),
         gulpif = require('gulp-if'),
-        livereload = require('gulp-livereload'),
         plumber = require('gulp-plumber')
         sass = require('gulp-sass'),
         csso = require('gulp-csso'),
         autoprefixer = require('gulp-autoprefixer');        
 
 /* Executable tasks */
-gulp.task('default', function() {
+gulp.task('default',['clean'], function() {
+    gulp.start('run');
+});
+
+gulp.task('run',['icons','images'], function() {
     gulp.start('sass');
 });
 
 gulp.task('dev', function() {
-    productionDefualtEnviorment = false;
+    config.productionEnvironment = false;
     gulp.start('default');
 });
 
 gulp.task('watch', function() {
-    productionDefualtEnviorment = false;
+    config.productionEnvironment = false;
     gulp.start('default');
-    livereload.listen();
-    gulp.watch([sourceRoot + 'scss/**/*.scss', './bower_components/**/*.scss'],['sass']);
+    gulp.watch([config.sourceDir + 'scss/**/*.scss', './bower_components/**/*.scss'],['sass']);
+});
+
+gulp.task('clean', function() {
+	return gulp.src(config.assetsDir, {read:false})
+		.pipe(clean());
+});
+
+gulp.task('images', function() { 
+    return gulp.src(config.sourceDir + '/images/**.*') 
+        .pipe(gulp.dest(config.assetsDir + '/images')); 
+});
+
+gulp.task('icons', function() { 
+    return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*') 
+        .pipe(gulp.dest(config.assetsDir + '/fonts')); 
 });
 
 /* Stylesheet compilation */
 gulp.task('sass', function () {
-    gulp.src(sourceRoot + 'scss/*.scss')
+    gulp.src(config.sourceDir + 'scss/*.scss')
         .pipe(plumber())
         .pipe(sourcemaps.init())
-        .pipe(sass())
+        .pipe(sass({
+        	includePaths: [
+        		config.bowerDir + 'bootstrap-sass-official/assets/stylesheets/',
+        		config.bowerDir + 'fontawesome/scss'
+        	]}
+        ))
         .pipe(autoprefixer({
             browsers: ['> 5% in NL'],
             cascade: true,
             remove: true
         }))
-        .pipe(gulpif(productionDefualtEnviorment, csso(), sourcemaps.write('../maps')))
-        .pipe(gulp.dest(assetsDirectory + 'css/'))
-        .pipe(livereload());
+        .pipe(gulpif(config.productionEnvironment, csso(), sourcemaps.write('../maps')))
+        .pipe(gulp.dest(config.assetsDir + 'css/'))
 });
