63957d243ba0f1d68561eb571afa046804880133
Arjen Smit <arjen@alnitak.nl>
2015-04-12 17:51:46 +0200

    Made it possible to add a different favicon and title for the production and development environment

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,24 +1,30 @@
 'use strict';
-var chatApp = angular.module('chatApp', ['ngRoute', 'chatControllers']);
+
+var chatApp = angular.module('chatApp', ['ngRoute', 'chatControllers', 'chatApp.config']);
+
+// Loading the config into the rootscope to be used in the main html document
+chatApp.run(function ($rootScope, config) {
+	$rootScope.config = config;
+});
 
 chatApp.config(['$routeProvider',
-	function($routeProvider) {
-		$routeProvider.		
-		when('/chat', {
-			templateUrl: '/assets/views/chat.html',
-			controller: 'chatCtrl'
-		}).
-		when('/login', {
-			templateUrl: '/assets/views/login.html',
-			controller: 'loginCtrl'
-		}).
-		when('/logout', {
-			templateUrl: '/assets/views/logout.html',
-			controller: 'logoutCtrl'
-		}).
-		otherwise({
-			redirectTo: '/login'
-		});
-	}]);
+    function($routeProvider) {
+        $routeProvider.
+            when('/chat', {
+                    templateUrl: '/assets/views/chat.html',
+                    controller: 'chatCtrl'
+            }).
+            when('/login', {
+                    templateUrl: '/assets/views/login.html',
+                    controller: 'loginCtrl'
+            }).
+            when('/logout', {
+                    templateUrl: '/assets/views/logout.html',
+                    controller: 'logoutCtrl'
+            }).
+            otherwise({
+                    redirectTo: '/login'
+            });
+    }]);
 
 var chatControllers = angular.module('chatControllers', ['firebase', 'chatApp.config', 'luegg.directives', 'favico.service', 'windowEventBroadcasts', 'yaru22.angular-timeago']);
