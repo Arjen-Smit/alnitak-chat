98310088e326d09823cc1233502f9986dce81192
Arjen Smit <arjen@alnitak.nl>
2015-04-15 22:33:18 +0200

    Added a user profile

--- src/javascript/controllers/chatCtrl.js
+++ src/javascript/controllers/chatCtrl.js
@@ -1,47 +1,54 @@
 'use strict';
 
-chatControllers.controller('chatCtrl', ['$scope', 'config', '$firebaseAuth', '$firebaseArray', '$location', 'favicoService', function($scope, config, $firebaseAuth, $firebaseArray, $location, favicoService) {
+chatControllers.controller('chatCtrl', ['$scope', 'config', '$firebaseAuth', '$firebaseArray', '$firebaseObject', '$location', 'favicoService', function($scope, config, $firebaseAuth, $firebaseArray, $firebaseObject, $location, favicoService) {
 	var ref = new Firebase(config.api);
  	var authData = ref.getAuth();
  	if (!authData) {
  		$location.path('/login');
- 	}
-    
- 	$scope.user = authData.google.cachedUserProfile;
+ 	} else {
 
- 	var messages = new Firebase(config.api + "messages");
-	$scope.messages = $firebaseArray(messages);
+        $scope.user = authData.google.cachedUserProfile;
 
-	var awayFor = 0;
-	var away = false;
+        var messages = new Firebase(config.api + "messages");
+        $scope.messages = $firebaseArray(messages);
 
-	$scope.$on('$windowFocus', function(broadcastEvent, browserEvent) {
-		awayFor = 0;
-		favicoService.badge(awayFor);
-		away = false;
-  	});
+        var profile = new Firebase(config.api + "users/" + authData.google.id);
+        var syncObject = $firebaseObject(profile);
+        syncObject.$bindTo($scope, "profile");
 
-  	$scope.$on('$windowBlur', function(broadcastEvent, browserEvent) {
-		awayFor = 0;
-		away= true;
-  	});
+        var awayFor = 0;
+        var away = false;
 
-	messages.on("value", function(snapshot) {
-		awayFor++;
-	  	if (awayFor !== 0 && away) {
-	  		favicoService.badge(awayFor);
-	  	}
-	});
+        $scope.$on('$windowFocus', function(broadcastEvent, browserEvent) {
+            awayFor = 0;
+            favicoService.badge(awayFor);
+            away = false;
+        });
 
-  	$scope.sendMessage = function() {
-  		if ($scope.text) {
-	  		$scope.messages.$add({
-  				'name': $scope.user.name,
-  				'message': $scope.text,
-  				'picture': $scope.user.picture,
-  				'timestamp': new Date().getTime()
-	  		});
-	  		$scope.text = '';
-  		}
-  	};
+        $scope.$on('$windowBlur', function(broadcastEvent, browserEvent) {
+            awayFor = 0;
+            away= true;
+        });
+
+        messages.on("value", function(snapshot) {
+            awayFor++;
+            if (awayFor !== 0 && away) {
+                favicoService.badge(awayFor);
+            }
+        });
+
+        $scope.sendMessage = function() {
+            if ($scope.text) {
+                $scope.messages.$add({
+                    id: $scope.user.id,
+                    name: $scope.user.name,
+                    message: $scope.text,
+                    picture: $scope.user.picture,
+                    timestamp: new Date().getTime()
+                });
+                $scope.text = '';
+            }
+        };
+
+    }
 }]);
--- src/javascript/controllers/loginCtrl.js
+++ src/javascript/controllers/loginCtrl.js
@@ -1,6 +1,6 @@
 'use strict';
 
-chatControllers.controller('loginCtrl', ['$scope', 'config', '$firebaseAuth', '$location', function($scope, config, $firebaseAuth, $location) {
+chatControllers.controller('loginCtrl', ['$scope', 'config', '$firebaseAuth', '$firebaseObject', '$location', function($scope, config, $firebaseAuth, $firebaseObject, $location) {
 	var authRef = new Firebase(config.api);
 	var auth = $firebaseAuth(authRef);
 	auth.$authWithOAuthPopup("google", {scope: "email"}).then(function(authData) {
@@ -11,6 +11,17 @@ chatControllers.controller('loginCtrl', ['$scope', 'config', '$firebaseAuth', '$
                 return false;
             }
         }
+
+        var profile = new Firebase(config.api + "users/" + authData.google.id);
+        var syncObject = $firebaseObject(profile);
+        syncObject.$bindTo($scope, "profile");
+
+        var userProfile = authData.google.cachedUserProfile;
+
+        profile.set({
+            name: userProfile.name,
+            picture: userProfile.picture
+        });
 
 		$location.path('/chat');
 
